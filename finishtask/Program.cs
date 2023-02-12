8789a6c3807f7a6a21af5e33498065d690237ef7
Stepan <tstepa037@gmail.com>
2023-02-12 19:06:09 +0300

    finishtask

---
+++ finishtask/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[8] {"result", "git", "run", "task", "nut", "push", "35", "rich"};
+string[] array2 = new string[array1.Length];
+void ChangeArray (string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ChangeArray(array1, array2);
+PrintArray(array2);
